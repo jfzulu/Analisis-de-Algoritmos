ad02afcea5fe6b83cfb6cf0be0345639b458353c
Nicolas <Vargas>
2022-08-31 20:45:56 -0500

    se añadio backtracking

--- taller3/Multiplicacion de matrices.jl
+++ taller3/Multiplicacion de matrices.jl
@@ -10,14 +10,14 @@
 
     for k in i + 1: j-1
  
-      cost = MM_Aux(dims, i, k,S)
+      numMult = MM_Aux(dims, i, k,S)
  
-      cost += MM_Aux(dims, k+1, j,S)
+      numMult += MM_Aux(dims, k+1, j,S)
  
-      cost += dims[i] * dims[k] * dims[j]
+      numMult += dims[i] * dims[k] * dims[j]
  
-      if cost < min
-        min = cost
+      if numMult < min
+        min = numMult
       end
     end
 
@@ -29,44 +29,62 @@
   return S[i,j]
 end=#
 
+function backtracking(B,s,e)
 
-function MM(d)
-    S = [0.0 for i=1:length(d), j = 1 : length(d)+1]   
-    B = [0.0 for i=1:length(d), j = 1 : length(d)+1]   
-    #return (MM_Aux(d,1,length(d),S))
-  
-    n = length(d)
-  
-  
-    for l in 2: n+1     
-   
-      for i in 1: n - l + 2
-   
-        j = i + l -1
-  
-        S[i,j] = Inf
-  
-   
-        k = i
-        while j < n && k <= j 
-          
-          cost = S[i,k] + S[k + 1,j] + d[i ] * d[k+1] * d[j+1]
-          
-          if cost < S[i,j]
-            S[i,j] = cost
-            B[i,j] = k
-          end
-          
-        k = k + 1
-          
-        end
-      end
-    end
-  
-    display(S)
-    display(B)
-    return S[1,n-1]
+  if s+1 == e
+
+    return "A"*string(s)*" A"*string(e)*""
+  end
+  if (s == e )
+
+     return "A"*string(s)
   end
   
-  d=[10,100,5,50]
-  println("El numero minimo de multiplicaciones es  "* string(MM(d)))
+  pos = B[s,e]
+  cadenaA = backtracking(B,s,pos)
+  cadenaB = backtracking(B,pos+1,e)
+
+  return "("*cadenaA*")("* cadenaB*")"
+  
+end
+
+
+function MM(d)
+  S = [0.0 for i=1:length(d), j = 1 : length(d)+1]   
+  B = [0.0 for i=1:length(d), j = 1 : length(d)+1]   
+  #return (MM_Aux(d,1,length(d),S))
+
+  n = length(d)
+
+
+  for l in 2: n+1     
+ 
+    for i in 1: n - l + 2
+ 
+      j = i + l -1
+
+      S[i,j] = Inf
+
+ 
+      k = i
+      while j < n && k <= j 
+        
+        numMult = S[i,k] + S[k + 1,j] + d[i ] * d[k+1] * d[j+1]
+        
+        if numMult < S[i,j]
+          S[i,j] = numMult
+          B[i,j] = k
+        end
+        
+      k = k + 1
+        
+      end
+    end
+  end
+  
+  println(backtracking(B, 1,length(d)-1))
+  return S[1,n-1]
+end
+
+d=[10,100,5,50]
+println("El numero minimo de multiplicaciones es  "* string(MM(d)))
